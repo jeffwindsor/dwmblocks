8d0c14c54d8eaf58678c3078b9f5e808c7b63c87
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-18 19:46:38 -0800

    Refactor

--- blocks.def.h
+++ blocks.def.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     { "", "~/.local/scripts/statusbar/clock",       5,  11 },
-    { "", "~/.local/scripts/statusbar/wifinetwork", 60, 12 },
-    { "", "~/.local/scripts/statusbar/volume",      20, 13 },
-    { "", "~/.local/scripts/statusbar/cpu",         5,  14 },
-    { "", "~/.local/scripts/statusbar/memory",      5,  15 },
-    { "", "~/.local/scripts/statusbar/disk",        60, 16 },
-    { "", "~/.local/scripts/statusbar/battery",     20, 17 },
+    //{ "", "~/.local/scripts/statusbar/wifinetwork", 60, 12 },
+    //{ "", "~/.local/scripts/statusbar/volume",      20, 13 },
+    { "", "~/.local/scripts/statusbar/cpu",         5,  14 },
+    { "", "~/.local/scripts/statusbar/memory",      5,  15 },
+    { "", "~/.local/scripts/statusbar/disk",        60, 16 },
+    { "", "~/.local/scripts/statusbar/battery",     20, 17 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " : ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
