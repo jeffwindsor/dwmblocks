dc7db1ca95d848dd16bc8dd5376d2ebbbd1c6b6a
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-20 16:38:10 -0800

    Refactor

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    { "", "~/.local/scripts/statusbar/clock",       5,  11 },
-    //{ "", "~/.local/scripts/statusbar/wifinetwork", 60, 12 },
-    //{ "", "~/.local/scripts/statusbar/volume",      20, 13 },
+    { "", "~/.local/scripts/statusbar/clock",        5,  11 },
+    { "", "~/.local/scripts/statusbar/wifinetwork", 60,  12 },
+    { "", "~/.local/scripts/statusbar/volume",      20,  13 },
     { "", "~/.local/scripts/statusbar/cpu",         5,  14 },
     { "", "~/.local/scripts/statusbar/memory",      5,  15 },
     { "", "~/.local/scripts/statusbar/disk",        60, 16 },
